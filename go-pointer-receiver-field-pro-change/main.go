5f74ea5c73822fe2d730367fafde9b06f6b00277
baba0119 <70310558+baba0119@users.noreply.github.com>
2022-02-06 14:13:05 +0900

    組込みインターフェース

--- go-pointer-receiver-field-pro-change/main.go
+++ go-pointer-receiver-field-pro-change/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+type samp interface {
+  sampleFunc(string, int, int)
+  samplePrint()
+}
+
 type sample struct {
   a string
   b int
@@ -16,14 +21,29 @@ func (sa *sample) sampleFunc(s string, i int, c int) {
   sa.c = c
 }
 
+func (sa *sample) samplePrint() {
+  fmt.Printf("%v, %v, %v\n", sa.a, sa.b, sa.c)
+  // data. 30, 40
+}
+
 func newSample() *sample {
   return &sample{}
 }
 
 func main() {
-	sa := newSample()
-  sa.sampleFunc("data", 30, 40)
 
-  fmt.Printf("%v, %v, %v\n", sa.a, sa.b, sa.c)
-  // data. 30, 40
+  // インターフェースでメソッドがちゃんと実装されているかを
+  // 確かめることができる
+  // インターフェースを介することで直接値を変更できない
+
+  var sa samp = newSample()
+  sa.sampleFunc("data", 30, 40)
+  sa.samplePrint()
 }
+
+/*
+外部パッケージに公開する時は
+インターフェースとインスタンス生成関数を公開しそこから取得する
+
+インターフェースとることで外部から使う際は構造体の中身を隠す
+*/
